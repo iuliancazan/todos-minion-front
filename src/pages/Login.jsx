f029d8a42b76f0ab4148fd9856419a7561a30f50
Iulian Cazan <iulian.cazan.81@gmail.com>
2022-02-23 02:46:10 +0200

    first commit b

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 function Login() {
-  return <div>Login</div>;
+  return <div>Login // work in progress</div>;
 }
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,5 @@
 function Register() {
-  return <div>Register</div>;
+  return <div>Register // work in progress</div>;
 }
 
 export default Register;
